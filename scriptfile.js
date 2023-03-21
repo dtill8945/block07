57f0eb1da6694ee5a6f7153a4022b32522938278
dtill8945 <dtill8945@gmail.com>
2023-03-20 19:48:31 -0500

    Updated

--- scriptfile.js
+++ scriptfile.js
@@ -5,9 +5,9 @@ alert("hello")
 console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
 
 //Give variables each an equation
-first = 5*2;
-second = 40/2;
-third = 19+10;
+first = 5 * 2;
+second = 40 / 2;
+third = 19 + 10;
 
 //Alerts user and gives them the combination to the secret vault
 alert(first + "-" + second + "-" + third);
