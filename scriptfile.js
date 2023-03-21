0da736e74391b7a8282f7a6db120d3dc19b09f33
dtill8945 <dtill8945@gmail.com>
2023-03-20 19:36:47 -0500

    Finished vault workshop

---
+++ scriptfile.js
@@ -0,0 +1,14 @@
+//Variables
+let first, second, third
+
+//Give user message regarding vault
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+//Give variables each an equation
+first = 5*2;
+second = 40/2;
+third = 19+10;
+
+//Alerts user and gives them the combination to the secret vault
+alert(first + "-" second + "-" + third);
+console.log(first + "-" second + "-" + third);
